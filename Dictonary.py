949798efdd21fa1e57bebcdb5038a173e421a201
Alina George <alinagrg03@gmail.com>
2025-01-01 15:42:22 +0530

    Update Dictonary.py

--- Dictonary.py
+++ Dictonary.py
@@ -1,9 +1,29 @@
-list1=[1,2,3,4,5]
+#creating dictionary
+dict1={"A1":'a',"A2":'b',"A3":'c',"A4":'d'}
+print(dict1)
 
-#to access an element
-a=list1[0]
+#accesing value using key
+key=input("Enter the key to access:")
+a=dict1[key]
 print(a)
 
-#to append into the list
-list1.append(6)
-print(list1)
+#to insert a key value pair
+key=input("Enter the key to insert:")
+val=input("Enter the value to insert:")
+dict1[key] = val
+print(dict1)
+
+#to remove a key-value pair
+key=input("Enter the key to delete:")
+del dict1[key]
+print(dict1)
+
+#to pop out an element
+pos=input("Enter the key to pop:")
+popp= dict1.pop(pos)
+print(dict1)
+print("The popped element is:",popp)
+
+#to get all the elements
+keys=dict1.keys()
+print("Gettin all the element",keys)
